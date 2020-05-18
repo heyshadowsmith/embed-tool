aafc91e92c80e58cacf9921bb757ec4962c082a1
Shadow Smith <shadow@businesscreditreports.com>
2020-05-18 14:23:55 -0500

    Add attribute

--- embed.js
+++ embed.js
@@ -2,4 +2,5 @@ const embed = document.getElementById('uw-8a64cm')
 
 const utility = document.createElement("div");
 utility.setAttribute("id", "utility");
+utility.createAttribute("v-html");
 embed.prepend(utility);
