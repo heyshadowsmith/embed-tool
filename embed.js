790cf00573cee971e862a2c391113be8932ef2b8
Shadow Smith <shadow@businesscreditreports.com>
2020-05-26 12:13:10 -0500

    Add various updates

--- embed.js
+++ embed.js
@@ -1,6 +1,23 @@
+// Add utility
 const embed = document.getElementById('uw-8a64cm')
+const utility = document.createElement("div")
+utility.setAttribute("id", "utility")
+vhtml = document.createAttribute("v-html")
+vhtml.value = "embed"
+utility.setAttributeNode(vhtml)
+embed.prepend(utility)
 
-const utility = document.createElement("div");
-utility.setAttribute("id", "utility");
-utility.createAttribute("v-html");
-embed.prepend(utility);
+const vueScript = document.createElement('script')
+vueScript.src = 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
+vueScript.onload = function () {
+  new Vue({
+    el: '#utility',
+    data() {
+      return {
+        embed: '<h2>Remote data</h2>'
+      }
+    }
+  })
+}
+
+document.body.appendChild(vueScript)
