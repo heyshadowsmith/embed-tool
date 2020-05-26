242a5e54e0efaf90bc170acb8edab4477c378b75
Shadow Smith <shadow@businesscreditreports.com>
2020-05-26 14:14:00 -0500

    Add alpine demo stump

---
+++ alpine.js
@@ -0,0 +1,31 @@
+async function getHtml() {
+  const response = await fetch('https://embed-tool.now.sh/alpine.html')
+  return response.text()
+}
+
+getHtml()
+.then(function (html) {
+
+  // Turn the markup into excecutable HTML
+  const parser = new DOMParser()
+  const utilityDocument = parser.parseFromString(html, 'text/html')
+  const utilityHtml = utilityDocument.body
+  document.getElementById('uw-test-widget').prepend(utilityHtml)
+})
+
+// Add Alpine
+const alpineScript = document.createElement('script')
+alpineScript.src = 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js'
+
+// When Alpine finishes loading add dependency function
+alpineScript.onload = function () {
+  function test() {
+    return {
+      hello() {
+        alert('hello')
+      }
+    }
+  }
+}
+
+document.head.prependChild(alpineScript)
