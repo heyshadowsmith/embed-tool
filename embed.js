03593bc60f3cfe745ecb224e7ea59f64df67fd84
Shadow Smith <shadow@businesscreditreports.com>
2020-05-26 12:34:56 -0500

    Fix script and update remote data

--- embed.js
+++ embed.js
@@ -1,22 +1,37 @@
-// Add utility
-const embed = document.getElementById('uw-8a64cm')
+// Create utility
+const embedHook = document.getElementById('uw-8a64cm')
 const utility = document.createElement("div")
 utility.setAttribute("id", "utility")
 vhtml = document.createAttribute("v-html")
 vhtml.value = "embed"
 utility.setAttributeNode(vhtml)
-embed.prepend(utility)
+embedHook.prepend(utility)
 
-const vueScript = document.createElement('script')
-vueScript.src = 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
-vueScript.onload = function () {
+async function getHtml() {
+  const response = await fetch('https://embed-tool-pbmvj2gh5.now.sh/remote.html')
+  return response.text()
+}
+
+function mountUtility(embed) {
   new Vue({
     el: '#utility',
     data() {
       return {
-        embed: '<h2>Remote data</h2>'
+        embed
       }
     }
+  })
+}
+
+// Add Vue
+const vueScript = document.createElement('script')
+vueScript.src = 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
+
+// Get utility HTML and mount the Vue instance
+vueScript.onload = function () {
+  getHtml()
+  .then(function (html) {
+    mountUtility(html)
   })
 }
 
